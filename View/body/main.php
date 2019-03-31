989f565be085942e3fa6ed44fde612e8aa28fbb6
Erichsu1224 <40513062+Erichsu1224@users.noreply.github.com>
2019-04-01 04:27:48 +0800

    Add files via upload

---
+++ View/body/main.php
@@ -0,0 +1,7 @@
+<p>
+  <div style="font-size: 18px; asign-text:center;">
+    <b>You can find which course your friend is taking.</b><br><br>
+    <b>And who is taking the same course with you!</b><br><br>
+    <b>Starts NOW!</b>
+  </div>
+</p>
---
+++ View/header/default.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title><?php if(isset($title)){ echo $title; }?></title>
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body topmargin="0" leftmargin="0">
+  <?php
+    require 'View/body/home.php';
+  ?>
